f438b5f9d81ad18d397de377c5ae50bc0e3813b5
RahulAmbesange <rahulambesange7@gmail.com>
2024-04-16 15:37:18 +0530

    PopUP using JS

---
+++ script.js
@@ -0,0 +1,20 @@
+// let popup=document.getElementById('popup');
+
+
+// function openPopUp(){
+// popup.classList.add('open-popup');
+// }
+// function closePopUp(){
+//    popup.classList.remove('open-popup');
+// }
+
+
+let popup=document.querySelector('.popup');
+
+function submitPop(){
+   popup.classList.add('showpop');
+}
+function cancelPop(){
+   popup.classList.remove('showpop');
+   // popup.classList.remove('returnpopup')
+}
